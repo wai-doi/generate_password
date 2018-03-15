192237e3b8616a8d4152c4fce8415a9a2d5352f1
Doi Yusuke <y_doi@nuee.nagoya-u.ac.jp>
2018-03-15 21:34:22 +0900

    first commit

---
+++ test/test_generate_password.rb
@@ -0,0 +1,40 @@
+require 'minitest/autorun'
+require_relative '../lib/generate_password'
+
+class TestGeneratePassword < Minitest::Test
+  SYMBOL_LIST = '._-=[]{}+#^!?'.chars.freeze
+  def setup
+    @password = Password.new.generate(16)
+    @symbol_password = Password.new(symbol: true).generate(16)
+  end
+
+  # 長さが32文字以下
+  def test_length_limit
+    assert @password.length <= 32
+    assert @symbol_password.length <= 32
+  end
+
+  # 0, o, 0は使わない
+  def test_unuse_zero_and_o
+    refute @password =~ /0|o|O/
+    refute @symbol_password =~ /0|o|O/
+  end
+
+  # 3文字以上連続しない
+  def test_sequence
+    refute @password =~ /(.)\1{3,}/
+    refute @symbol_password =~ /(.)\1{3,}/
+  end
+
+  # オプションをつけた場合には1、2こ記号を使う
+  def test_use_symbol
+    symbol_count = @symbol_password.scan(Regexp.union(SYMBOL_LIST)).size
+    assert [1, 2].include?(symbol_count)
+  end
+
+  # 最初と最後の文字には記号は使わない
+  def test_unuse_symbol_at_head_and_tail
+    refute @symbol_password[0] =~ Regexp.union(SYMBOL_LIST)
+    refute @symbol_password[-1] =~ Regexp.union(SYMBOL_LIST)
+  end
+end
